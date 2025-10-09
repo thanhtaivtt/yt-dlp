773c1061f8533dca4c2154eb2bdbfb1074eea871
thanhtaivtt <thanhtai96nd@gmail.com>
2025-10-09 22:07:25 +0700

    Revert "add remove emoji in video title"
    This reverts commit b5a02c5ac2f0cc924adb23f02361e61c0eac7021.

--- yt_dlp/YoutubeDL.py
+++ yt_dlp/YoutubeDL.py
@@ -174,7 +174,6 @@ from .utils.networking import (
     clean_proxies,
     std_headers,
 )
-from .utils.standardize_text import StandardizeText
 from .version import CHANNEL, ORIGIN, RELEASE_GIT_HEAD, VARIANT, __version__
 
 if os.name == 'nt':
@@ -1822,11 +1821,13 @@ class YoutubeDL:
             })
 
     def process_ie_result(self, ie_result, download=True, extra_info=None):
-        # remove emoji for fix decode in Windows
-        if ie_result is not None:
-            standardize = StandardizeText(ie_result.get('title'))
-            ie_result['title'] = standardize.standardize()
+        """
+        Take the result of the ie(may be modified) and resolve all unresolved
+        references (URLs, playlist items).
 
+        It will also download the videos if 'download'.
+        Returns the resolved ie_result.
+        """
         if extra_info is None:
             extra_info = {}
         result_type = ie_result.get('_type', 'video')
