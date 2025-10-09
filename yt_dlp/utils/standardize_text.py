e051b7051a32bdc5c544f625579469dacb956673
thanhtaivtt <thanhtai96nd@gmail.com>
2025-10-09 22:07:21 +0700

    Revert "update logic remove characters"
    This reverts commit 8469b5c69cbba8a6648ba20295ef28da17947954.

--- yt_dlp/utils/standardize_text.py
+++ yt_dlp/utils/standardize_text.py
@@ -21,15 +21,15 @@ def remove_emoji(text):
     return emoji_pattern.sub(r'', text)
 
 
-def remove_special_chars(text):
+def remove_dot(text):
     if text is None:
         return text
 
-    return re.sub(r'[.,\'"#]', '', text)  # Removes ., ', ", and #
+    return re.sub(r'\.', '', text)
 
 
 def standardize_text(text):
-    return remove_special_chars(remove_emoji(text))
+    return remove_dot(remove_emoji(text))
 
 
 class StandardizeText:
